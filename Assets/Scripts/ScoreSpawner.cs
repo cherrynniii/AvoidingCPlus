62f3b3d520384ee827393c2328a28f71c14e9070
cherrynniii <chaelin20760@gmail.com>
2025-11-11 17:51:35 +0900

    feat#5: 점수 무한 랜덤 생성 후 낙하

---
+++ Assets/Scripts/ScoreSpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject[] scores; // A+, A0, B+, B0, C+ 객체
+    private float[] arrPosX = {-2.2f, -1.1f, 0f, 1.1f, 2.2f};   // 객체 생성 x 위치
+
+    [SerializeField]
+    private float spawnInterval;  // 객체 생성 주기
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartScoreRoutine();
+    }
+
+    // coroutine
+    void StartScoreRoutine() {
+        StartCoroutine("ScoreRoutine");
+    }
+
+    IEnumerator ScoreRoutine() {
+        yield return new WaitForSeconds(3f);
+
+        while (true) {
+            float posX = arrPosX[Random.Range(0, arrPosX.Length)];
+            int index = Random.Range(0, scores.Length);     // score 객체 랜덤 뽑기
+            SpawnScore(posX, index);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+
+    // 스폰 x 위치, 어떤 score 객체 만들지 인덱스
+    void SpawnScore(float posX, int index) {
+        Vector3 spawnPos = new Vector3(posX, transform.position.y, transform.position.z);
+        Instantiate(scores[index], spawnPos, Quaternion.identity);
+    }
+}
